45b360920df9342d00f7d3993ac3c736edc06676
Ben Timby <btimby@gmail.com>
2013-04-22 11:22:12 -0400

    Version bump, removed download URL (github).

--- setup.py
+++ setup.py
@@ -5,11 +5,9 @@ from distutils.core import setup
 
 name = 'dropboxfs'
 version = '0.1'
-release = '7'
+release = '10'
 versrel = version + '-' + release
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
-download_url = 'https://github.com/downloads/btimby/fs-dropbox' \
-                           '/' + name + '-' + versrel + '.tar.gz'
 long_description = file(readme).read()
 
 setup(
@@ -26,7 +24,6 @@ setup(
     maintainer = 'Ben Timby',
     maintainer_email = 'btimby@gmail.com',
     url = 'http://github.com/btimby/fs-dropbox/',
-    download_url = download_url,
     license = 'GPLv3',
     py_modules=['dropboxfs'],
     package_data={'': ['README.rst']},
